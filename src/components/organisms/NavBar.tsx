aaf9e7fde27d454d670347b3a41c5061a7c78adc
WalidAra <arawalid90@gmail.com>
2024-05-24 14:29:35 +0100

    test

--- src/components/organisms/NavBar.tsx
+++ src/components/organisms/NavBar.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const NavBar = () => {
   return (
-    <header className="fixed flex w-full flex-col gap-3 bg-white/95 p-3 backdrop-blur supports-[backdrop-filter]:bg-white/60 md:h-16 md:flex-row md:items-center lg:px-4">
-      <img src="/logo.png" alt="logo" className="w-14 " />
+    <header className="fixed flex z-30 w-full flex-col gap-3 bg-white/95 p-3 backdrop-blur supports-[backdrop-filter]:bg-white/60 md:h-16 md:flex-row md:items-center lg:px-4">
+      {/* <img src="/logo.png" alt="logo" className="w-14 " /> */}
     </header>
   );
 };
