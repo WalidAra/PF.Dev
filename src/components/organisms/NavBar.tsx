be0a1b3f0d2b4cbc2a25a4be46da56823ae5c393
WalidAra <arawalid90@gmail.com>
2024-05-24 14:37:33 +0100

    added logo

--- src/components/organisms/NavBar.tsx
+++ src/components/organisms/NavBar.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const NavBar = () => {
   return (
     <header className="fixed flex z-30 w-full flex-col gap-3 bg-white/95 p-3 backdrop-blur supports-[backdrop-filter]:bg-white/60 md:h-16 md:flex-row md:items-center lg:px-4">
-      {/* <img src="/logo.png" alt="logo" className="w-14 " /> */}
+      <img src="/logo.png" alt="logo" className="w-14 " />
     </header>
   );
 };
