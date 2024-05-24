d8ca115f27ac52ef855f2afa501f8c647570f615
WalidAra <arawalid90@gmail.com>
2024-05-24 14:31:56 +0100

    recover ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import "./styles/tail.css";
-// import MainView from "./components/organisms/MainView";
+import MainView from "./components/organisms/MainView";
 import NavBar from "./components/organisms/NavBar";
-// import PromptResult from "./components/organisms/PromptResult";
-// import FormProvider from "./providers/FormProvider";
+import PromptResult from "./components/organisms/PromptResult";
+import FormProvider from "./providers/FormProvider";
 
 function App() {
   return (
     <div className="w-full min-h-screen ">
       <NavBar />
-      {/* <FormProvider>
+      <FormProvider>
         <MainView />
         <PromptResult />
-      </FormProvider> */}
+      </FormProvider>
     </div>
   );
 }
